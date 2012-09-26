9dd03103dd699b3cd9b67e4fc9673aa3c19859a6
Espen <espenhellerud5@gmail.com>
2012-09-26 23:17:57 +0200

    Fylt ut GpsData

--- src/sw/server/GpsData.java
+++ src/sw/server/GpsData.java
@@ -1,5 +1,22 @@
 package sw.server;
 
 public class GpsData {
-
+	private double latitude;//positive north for equator, negative south
+	private double longitude; //positive east for Prime Meridian, negative west
+	
+	public GpsData(){
+		this.latitude=0;
+		this.longitude=0;
+	}
+	public GpsData(double latitude, double longitude){
+		this.latitude=latitude;
+		this.longitude=longitude;
+	}
+	public double getLatitude() {
+		return latitude;
+	}
+	public double getLongitude() {
+		return longitude;
+	}
+	
 }
